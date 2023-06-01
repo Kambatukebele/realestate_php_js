f077c4bc0e105f2746a582cd8b2fc3f7a7ae5af8
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-06-01 10:24:31 +0200

    update

--- app/views/theme/home.view.php
+++ app/views/theme/home.view.php
@@ -27,144 +27,186 @@
     <main>
       <!--HEADER  -->
       <header class="header">
-        <div class="wrapper">
-          <div class="header__all box-all">
-            <div class="header__logo">
-              <img class="header__logo--img" src="<?=ROOT_ASSETS?>theme/images/logo1.png" alt="logo" />
-            </div>
-            <nav class="header__nav">
-              <ul class="header__ul">
-                <a href="#" class="header_link">
-                  <li class="header__li">Home</li>
-                </a>
-                <a href="#" class="header_link">
-                  <li class="header__li">Buy</li>
-                </a>
-                <a href="#" class="header_link">
-                  <li class="header__li">Sell</li>
-                </a>
-                <a href="#" class="header_link">
-                  <li class="header__li">Listing</li>
-                </a>
-                <a href="#" class="header_link">
-                  <li class="header__li">Contact</li>
-                </a>
-              </ul>
-            </nav>
-            <div class="header__auth">
-              <div class="header__auth--all">
-                <div class="header__auth--signup">
-                  <a class="header__auth--signup-link href=" #"><button class="header__auth--signup-btn"
-                      type="button">Signup</button></a>
-                </div>
-                <div class="header__auth--login">
-                  <a class="header__auth--login-link href=" #"><button class="header__auth--login-btn"
-                      type="button">Login</button></a>
-                </div>
+        <div class="header__wrapper">
+          <div class="header__logo">
+            <img class="header__logo--img" src="<?=ROOT_ASSETS?>theme/images/logo1.png" alt="logo" />
+          </div>
+          <nav class="header__nav">
+            <ul class="header__ul">
+              <a href="#" class="header_link">
+                <li class="header__li">Home</li>
+              </a>
+              <a href="#" class="header_link">
+                <li class="header__li">Buy</li>
+              </a>
+              <a href="#" class="header_link">
+                <li class="header__li">Sell</li>
+              </a>
+              <a href="#" class="header_link">
+                <li class="header__li">Listing</li>
+              </a>
+              <a href="#" class="header_link">
+                <li class="header__li">Contact</li>
+              </a>
+            </ul>
+          </nav>
+          <div class="header__auth">
+            <div class="header__auth--all">
+              <div class="header__auth--signup">
+                <a class="header__auth--signup-link href=" #"><button class="header__auth--signup-btn"
+                    type="button">Signup</button></a>
+              </div>
+              <div class="header__auth--login">
+                <a class="header__auth--login-link href=" #"><button class="header__auth--login-btn"
+                    type="button">Login</button></a>
               </div>
             </div>
           </div>
         </div>
       </header>
+      <!--HAMBURBUR MENU  -->
+      <section class="hamburgerMenu">
+        <div class="hamburgerMenu__wrapper">
+          <div class="hamburgerMenu__logo">
+            <img class="hamburgerMenu__logo--img" src="<?=ROOT_ASSETS?>theme/images/logo1.png" alt="logo" />
+          </div>
+          <div class="hamburgerMenu__group">
+            <div class="hamburgerMenu__menu">
+              <span class="material-symbols-outlined">
+                menu
+              </span>
+            </div>
+            <div class="hamburgerMenu__close">
+              <span class="material-symbols-outlined">
+                close
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- HAMBURGER LIST -->
+
+      <!-- ENDHAMBURGER LIST -->
+      <!--END HAMBURBUR MENU  -->
+      <div class="hamburgerMenu__list">
+        <a href="#">
+          <li>Home</li>
+        </a>
+        <a href="#">
+          <li>Buy</li>
+        </a>
+        <a href="#">
+          <li>Sell</li>
+        </a>
+        <a href="#">
+          <li>Listing</li>
+        </a>
+        <a href="#">
+          <li>Contact</li>
+        </a>
+        <a href="#">
+          <li><button>Signup</button></li>
+        </a>
+        <a href="#">
+          <li><button>Login</button></li>
+        </a>
+      </div>
       <!-- END HEADER  -->
       <!-- HERO -->
       <section class="hero">
-        <div class="hero__wrapper wrapper">
-          <div class="hero__all box-all">
-            <div class="hero__text">
-              <h1 class="hero__title">We will help you find <br>
-                your <span class="hero__title--green">Wonderful</span> home</h1>
-              <p class="hero_description">A great plateform to buy, sell and rent your properties without any agent or
-                commisions.</p>
-            </div>
+        <div class="hero__wrapper">
+          <div class="hero__text">
+            <h1 class="hero__title">We will help you find <br>
+              your <span class="hero__title--green">Wonderful</span> home</h1>
+            <p class="hero_description">A great plateform to buy, sell and rent your properties without any agent or
+              commisions.</p>
           </div>
         </div>
       </section>
       <!-- END HERO -->
       <!--SEARCH  -->
       <section class="search">
-        <div class="search__wrapper wrapper">
-          <div class="search__all box-all">
-            <div class="search__buy-sell-rent">
-              <div class="search__buy-sell-rent--left">
-                <span class="search__buy search__buy-sell-rent--active"><a href="#"><button
-                      type="button">Buy</button></a></span>
-                <span class="search__sell"><a href="#"><button type="button">Sell</button></a></span>
-                <span class="search__rent "><a href="#"><button type="button">Rent</button></a></span>
+        <div class="search__wrapper">
+          <div class="search__buy-sell-rent">
+            <div class="search__buy-sell-rent--left">
+              <span class="search__buy search__buy-sell-rent--active"><a href="#"><button
+                    type="button">Buy</button></a></span>
+              <span class="search__sell"><a href="#"><button type="button">Sell</button></a></span>
+              <span class="search__rent "><a href="#"><button type="button">Rent</button></a></span>
+            </div>
+          </div>
+          <div class="search__form--all">
+            <form method="post">
+              <div class="search__form">
+                <div class="search__form--box">
+                  <div class="search__form--text">
+                    <h4 class="search__form--title">Search : <small>*</small></h4>
+                  </div>
+                  <div class="search__form--input">
+                    <span class="material-symbols-outlined">
+                      search
+                    </span>
+                    <input type="text" name="" id="" placeholder="Search your keywords">
+                  </div>
+                </div>
+                <div class="search__form--box">
+                  <div class="search__form--text">
+                    <h4 class="search__form--title">Select Categories : <small>*</small></h4>
+                  </div>
+                  <div class="search__form--input">
+                    <span class="material-symbols-outlined">
+                      home
+                    </span>
+                    <select name="" id="">
+                      <option value="">Houses</option>
+                      <option value="">Appartment</option>
+                      <option value="">Offices</option>
+                      <option value="">TownOffice</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="search__form--box">
+                  <div class="search__form--text">
+                    <h4 class="search__form--title">Min Price : <small>*</small></h4>
+                  </div>
+                  <div class="search__form--input">
+                    <span class="material-symbols-outlined">
+                      monetization_on
+                    </span>
+                    <select name="" id="">
+                      <option value="">Min Price</option>
+                      <option value="">500</option>
+                      <option value="">1000</option>
+                      <option value="">1500</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="search__form--box">
+                  <div class="search__form--text">
+                    <h4 class="search__form--title">Max Price : <small>*</small></h4>
+                  </div>
+                  <div class="search__form--input">
+                    <span class="material-symbols-outlined">
+                      monetization_on
+                    </span>
+                    <select name="" id="">
+                      <option value="">Max Price</option>
+                      <option value="">500</option>
+                      <option value="">1000</option>
+                      <option value="">1500</option>
+                    </select>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div class="search__form--all">
-              <form method="post">
-                <div class="search__form">
-                  <div class="search__form--box">
-                    <div class="search__form--text">
-                      <h4 class="search__form--title">Search : <small>*</small></h4>
-                    </div>
-                    <div class="search__form--input">
-                      <span class="material-symbols-outlined">
-                        search
-                      </span>
-                      <input type="text" name="" id="" placeholder="Search your keywords">
-                    </div>
-                  </div>
-                  <div class="search__form--box">
-                    <div class="search__form--text">
-                      <h4 class="search__form--title">Select Categories : <small>*</small></h4>
-                    </div>
-                    <div class="search__form--input">
-                      <span class="material-symbols-outlined">
-                        home
-                      </span>
-                      <select name="" id="">
-                        <option value="">Houses</option>
-                        <option value="">Appartment</option>
-                        <option value="">Offices</option>
-                        <option value="">TownOffice</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div class="search__form--box">
-                    <div class="search__form--text">
-                      <h4 class="search__form--title">Min Price : <small>*</small></h4>
-                    </div>
-                    <div class="search__form--input">
-                      <span class="material-symbols-outlined">
-                        monetization_on
-                      </span>
-                      <select name="" id="">
-                        <option value="">Min Price</option>
-                        <option value="">500</option>
-                        <option value="">1000</option>
-                        <option value="">1500</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div class="search__form--box">
-                    <div class="search__form--text">
-                      <h4 class="search__form--title">Max Price : <small>*</small></h4>
-                    </div>
-                    <div class="search__form--input">
-                      <span class="material-symbols-outlined">
-                        monetization_on
-                      </span>
-                      <select name="" id="">
-                        <option value="">Max Price</option>
-                        <option value="">500</option>
-                        <option value="">1000</option>
-                        <option value="">1500</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-                <div class="search__form--button">
-                  <button type="submit">Search</button>
-                </div>
-              </form>
-            </div>
+              <div class="search__form--button">
+                <button type="submit">Search</button>
+              </div>
+            </form>
           </div>
         </div>
       </section>
       <!--END SEARCH  -->
+
     </main>
     <!-- SCRIPTS -->
     <script src="<?=ROOT_ASSETS?>theme/js/main.js"></script>
