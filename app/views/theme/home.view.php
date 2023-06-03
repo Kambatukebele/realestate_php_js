9ab8dbba95606647bf3214ae98b3beee1a9e9569
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-06-03 19:23:54 +0200

    update

--- app/views/theme/home.view.php
+++ app/views/theme/home.view.php
@@ -974,7 +974,100 @@
       <footer class="footer">
         <div class="footer__wrapper">
           <div class="footer__logo">
+            <a href="#"><img src="<?=ROOT_ASSETS?>theme/images/logo2.png" alt="logo"></a>
+            <p>A great plateform to buy, sell and rent your properties without any agent or commisions.</p>
+          </div>
 
+          <div class="footer__company">
+            <div class="footer__company--title">
+              <h4>Company</h4>
+            </div>
+            <div class="footer__company__list">
+              <a href="#">
+                <li>About us</li>
+              </a>
+              <a href="#">
+                <li>Services</li>
+              </a>
+              <a href="#">
+                <li>Pricing</li>
+              </a>
+              <a href="#">
+                <li>Blog</li>
+              </a>
+              <a href="#">
+                <li>Login</li>
+              </a>
+            </div>
+          </div>
+
+          <div class="footer__company">
+            <div class="footer__company--title">
+              <h4>Usefull Links</h4>
+            </div>
+            <div class="footer__company__list">
+              <a href="#">
+                <li>Terms of Services</li>
+              </a>
+              <a href="#">
+                <li>Privacy Policy</li>
+              </a>
+              <a href="#">
+                <li>Listing</li>
+              </a>
+              <a href="#">
+                <li>Contact</li>
+              </a>
+            </div>
+          </div>
+
+          <div class="footer__company2">
+            <div class="footer__company--title">
+              <h4>Contact Details</h4>
+            </div>
+            <div class="footer__company__adresses">
+              <div class="footer__company__adress--location">
+                <div class="footer__company__adress--icon">
+                  <span class="material-symbols-outlined">
+                    location_on
+                  </span>
+                </div>
+                <div class="footer__company__adress--adress">
+                  <h4>C/54 Northwest Freeway,<br>
+                    Suite 558,<br>
+                    Houston, USA 485 <br></h4>
+
+                  <a href="#">View on Google map</a>
+                </div>
+              </div>
+              <div class="footer__company__adress--contact">
+                <div class="footer__company__adress--icon">
+                  <span class="material-symbols-outlined">
+                    mail
+                  </span>
+                </div>
+                <div class="footer__company__adress--adress">
+                  contact@example.com
+                </div>
+              </div>
+
+              <div class="footer__company__adress--call">
+                <div class="footer__company__adress--icon">
+                  <span class="material-symbols-outlined">
+                    call
+                  </span>
+                </div>
+                <div class="footer__company__adress--adress">
+                  +152 534-468-854
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <div class="footer__wrapper2">
+          <div class="footer__designedBy">
+            <p>©<?= date("Y"); ?> Hously. Design & Develop with by Webizy.</p>
           </div>
         </div>
       </footer>
